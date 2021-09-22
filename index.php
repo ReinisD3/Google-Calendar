d69a21bdaff40b5ff6c219e04c2e4c5a3f054483
ReinisD3 <reinis.detlavs.com>
2021-09-22 08:58:13 +0300

    first commit

---
+++ index.php
@@ -0,0 +1,62 @@
+
+<!DOCTYPE html>
+<html>
+<body>
+<h1>Event link generator</h1>
+
+<form action="/" method="post">
+    <label for="title">Title:</label><br>
+    <input type="text" id="title" name="title" ><br>
+    <label for="from">From:</label><br>
+    <input type="text" id="from" name="from"><br>
+    <label for="to">To:</label><br>
+    <input type="text" id="to" name="to"><br>
+    <label for="from">Description:</label><br>
+    <input type="text" id="description" name="description"><br>
+    <label for="from">Address:</label><br>
+    <input type="text" id="address" name="address"><br><br><br>
+    <input type="checkbox" id="linkGoogle" name="linkGoogle" value="google">
+    <label for="vehicle1"> Google Link</label>
+    <input type="checkbox" id="linkYahoo" name="linkYahoo" value="yahoo">
+    <label for="vehicle2"> Yahoo Link</label>
+    <input type="checkbox" id="linkOutlook" name="linkOutlook" value="webOutlook">
+    <label for="vehicle3"> WebOutlook</label><br><br>
+
+    <input type="submit"  name="submit" value="Generate">
+</form>
+
+
+</body>
+</html>
+
+<?php
+
+require_once 'vendor/autoload.php';
+
+use Spatie\CalendarLinks\Link;
+
+
+if (isset($_POST['title']) && isset($_POST['from']) && isset($_POST['to']))
+{
+
+    $linkType = [];
+    if(isset($_POST['linkGoogle'])) $linkType[]=$_POST['linkGoogle'];
+    if(isset($_POST['linkYahoo'])) $linkType[]=$_POST['linkYahoo'];
+    if(isset($_POST['linkOutlook'])) $linkType[]=$_POST['linkOutlook'];
+
+    foreach ($linkType as $type)
+    {
+        $link = Link::create(
+            $_POST['title'],
+            DateTime::createFromFormat('Y-m-d H:i', $_POST['from']),
+            DateTime::createFromFormat('Y-m-d H:i', $_POST['to'])
+        )
+            ->description($_POST['description'])
+            ->address($_POST['address'])
+            ->$type();
+        echo 'Event link for '.$type.':';
+        echo "<br><li><a href='$link'>$link</a> </li> ";
+    }
+
+}
+?>
